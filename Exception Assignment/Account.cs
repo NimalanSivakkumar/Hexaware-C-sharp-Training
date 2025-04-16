bc8a2a875b6d6d8b61f4bea2aa6c945f0e4c4134
Nimalan Sivakkumar <158604955+NimalanSivakkumar@users.noreply.github.com>
2025-04-16 21:55:28 +0530

    Add files via upload

---
+++ Exception Assignment/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_assignment
+{
+    public class Account : IComparable<Account>
+    {
+        public int AccountNumber {  get; set; }
+        public string Name { get; set; }
+        public double Balance {  get; set; }
+        public string AccountType {  get; set; }
+
+        public Account(int accNo,string name,double balance,string accounttype) 
+        { 
+            this.AccountNumber = accNo;
+            this.Name = name;
+            this.Balance = balance;
+            this.AccountType = accounttype;
+
+        }
+
+        public void Deposit(double amount)
+
+
+
+
+
+
+
+    }
+}
---
+++ Exception Assignment/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Exception_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                HMBank acc1 = new HMBank("9874", 6000,"savings",1000);
+                HMBank acc2 = new HMBank("9875", 8000, "current", 2000);
+
+            
+                    acc1.Display();
+                    acc2.Display();
+
+
+                try
+                {
+                    acc1.Withdraw(8000);
+                }
+                catch (InsufficientFundException e)
+                {
+                    Console.WriteLine($"Insuff fund:{e.Message}");
+                }
+
+                try
+                {
+                    acc1.Transfer(7000.00, acc2.account_num);
+                }
+                catch (InsufficientFundException e)
+                {
+                    Console.WriteLine($"Insuff fund:{e.Message}");
+                }
+
+                try
+                {
+                    acc2.Transfer(3000.00, "123456789");
+                }
+
+                catch(InvalidAccountException e)
+                { 
+            
+                    Console.WriteLine($"Invalid acc:{e.Message}");
+                }
+                try
+                {
+
+                    acc2.Withdraw(1500.00);
+                }
+                catch (OverDraftLimitExcededException e)
+                {
+                    Console.WriteLine($"Overlimit:{e.Message}");
+                }
+            
+
+           
+
+            
+
+          
+
+          
+
+
+        }
+    }
+}
