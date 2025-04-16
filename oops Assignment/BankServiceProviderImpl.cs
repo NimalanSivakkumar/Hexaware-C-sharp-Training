db56601a053eb3974af6542fd0ce4bd1a264764e
Nimalan Sivakkumar <158604955+NimalanSivakkumar@users.noreply.github.com>
2025-04-16 21:53:40 +0530

    Add files via upload

---
+++ oops Assignment/BankServiceProviderImpl.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banking_asssignment
+{
+    public class BankServiceProviderImpl : CustomerServiceProviderImpl, IBankServiceProvider
+    {
+        public string BranchName {  get; set; }
+
+
+        public string BranchAddress{ get; set; }
+
+        public BankServiceProviderImpl(string branchName,string branchAddress)
+        {
+            BranchName = branchName;
+            BranchAddress = branchAddress;
+        }
+        
+
+
+
+        public void CreateAccount(Customer customer, String accountType, float balance)
+        {
+            Account newAccount = accountType switch
+
+            //accountType = accountType.ToLower();
+
+            //switch (accountType)
+            {
+                "savings" => new SavingsAccount(customer, balance),
+
+
+
+                "current" => new CurrentAccount(customer, balance),
+
+
+                "zerobalance" =>
+                    new ZeroBalanceAccount(customer, balance),
+
+
+                _ =>
+                     throw new InvalidAccountException("Invalid account")
+
+            };
+
+
+                accounts.Add(newAccount);
+                Console.WriteLine($"Acc created : {newAccount.AccountNumber} ");
+
+
+            }
+            
+        
+
+        public void ListAccounts()
+        {
+            if(accounts.Count() == 0)
+            {
+                throw new InvalidAccountException("Invalid Account bro");
+            }
+            else
+            {
+                foreach (var account in accounts)
+                {
+                    Console.WriteLine(account.ToString());
+                }
+            }
+               
+        }
+
+
+        public void CalculateInterest()
+        {
+           
+            foreach(var account in accounts)
+            {
+                if(account is SavingsAccount savingsAccount)
+                {
+                    float interest = savingsAccount.AccountBalance * SavingsAccount.interestRate;
+                    savingsAccount.AccountBalance += interest;
+                    Console.WriteLine($"Interest calculated,amount:{interest}");
+
+                }
+                else
+                {
+                    throw new InvalidAccountException("Account type is wrong");
+                }
+                
+            }
+
+        }
+
+
+    }
+}
---
+++ oops Assignment/CustomerServiceProviderImpl.cs
@@ -0,0 +1,63 @@
+﻿namespace banking_asssignment
+{
+   public class CustomerServiceProviderImpl : ICustomerServiceProvider
+   {
+        public List<Account> accounts = new List<Account>();
+
+
+     
+
+        public Account FindAccount(long accNo)
+        {
+            var account = accounts.Find(acc => acc.AccountNumber == accNo);
+            if (account == null)
+            {
+                throw new InvalidAccountException("Account bro");
+            }
+            return account;
+        }
+
+        public float GetAccountBalance(long AccountNumber)
+        {
+            
+            return FindAccount(AccountNumber).AccountBalance;
+
+        }
+
+        public void Deposit(long AccountNumber,float Amount)
+        {
+           var acc = FindAccount(AccountNumber);
+            acc.Deposit(AccountNumber,Amount);
+            Console.WriteLine(acc.AccountBalance);  
+
+        }
+
+        public void Withdraw(long AccountNumber, float Amount)
+        {
+            var acc = FindAccount(AccountNumber);
+            acc.Withdraw(AccountNumber, Amount);
+            Console.WriteLine(acc.AccountBalance); 
+
+
+        }
+
+        public void Transfer(long fromAccountNumber, int toAccountNumber, float amount)
+        {
+            var fromAccount = FindAccount(fromAccountNumber);
+            var toAccount = FindAccount(toAccountNumber);
+            fromAccount.Withdraw(fromAccountNumber,amount);
+            toAccount.Deposit(toAccountNumber,amount);
+           
+        }
+
+       public void GetAccountDetails(long AccountNumber)
+        {
+            var acc = FindAccount(AccountNumber);
+
+            Console.WriteLine(acc.ToString());
+
+        }
+
+   
+    }
+}
